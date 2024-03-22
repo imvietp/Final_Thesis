b59b370dcbadd281cc6517fe017e9600815da76b
Phạm Việt <brogun2002@gmail.com>
2024-03-23 01:19:26 +0700

    push flame sensor

---
+++ Code/flame_sensor/flame_sensor.ino
@@ -0,0 +1,24 @@
+#define flame_sensor 5
+#define led 18
+
+
+void setup()
+{
+  pinMode(flame_sensor, INPUT); //initialize Flame sensor output pin connected pin as input.
+  pinMode(led, OUTPUT);// initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(9600);// initialize serial communication @ 9600 baud:
+}
+void loop()
+{
+  if (digitalRead(flame_sensor) == 0 )
+  {
+    digitalWrite(led, HIGH); // Led ON
+    Serial.println("** Fire detected!!! **");
+  }
+  else
+  {
+    digitalWrite(led, LOW); // Led OFF
+    Serial.println("No Fire detected");
+  }
+  delay(100);
+}
